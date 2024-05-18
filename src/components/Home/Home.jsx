6d3a423c2a0bd2bff8998c94b5d462f3bb612904
premvarma2002 <vakilverma1982@gmail.com>
2024-05-18 17:56:03 +0530

    Add Hero banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,10 +6,20 @@ const Home = () => {
     <>
     <div className='home-scss'>
       <Header/>
-    {/* <div>Contents</div>
-    <div>Contents</div>
-    <div>Contents</div>
-    <div>Contents</div> */}
+    <div className="hero-content">
+      <div>
+        <h5 className="desc">The Easiest Way  <br /> To Make Your  <br /> Favourite Meal</h5>
+       <div className="para">  
+        <p>Discover 1000+ best recipes in your hand with the best recipe.</p>
+        <p>Help you to finf the easiest way to cook.</p>
+       
+       <button className="btn ">Explore Recipes</button>
+       </div>
+      </div>
+      <div className="image">
+        <img src="./heroimg.png" alt="hero image" size={100} />
+      </div>
+    </div>
     </div>
     </>
   )

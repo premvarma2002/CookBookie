633bfde7cd92aa11ac8eee8cf4998f5d7ebe0698
premvarma2002 <vakilverma1982@gmail.com>
2024-05-19 13:19:52 +0530

    Add Newsletter and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Footer from "./components/Footer/Footer"
 import Home from "./components/Home/Home"
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+
 
 const App = () => {
   return (
     <>
     <Home/>
+    <Newsletter/>
     <Footer/>
     </>
   )

1c4d0a864234f194eb6d54bcf6996b8f16dc32ac
premvarma2002 <vakilverma1982@gmail.com>
2024-05-28 13:55:56 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Footer from "./components/Footer/Footer"
 import Home from "./pages/Home/Home"
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
 import RecipeLists from "./components/RecipeList/RecipeLists";
 import Banner from "./components/Banner/Banner";
 // import Community from "./components/Community/Community";
 import DownloadApp from "./components/DownloadApp/DownloadApp";
 import Header from "./components/Header/Header";
+import Favourites from "./pages/Favourites/Favourites";
+import Recipes from "./pages/Recipes/Recipes";
 // import { useState } from "react";
 
 
@@ -13,18 +16,21 @@ const App = () => {
   // const [loader,setLoader] = useState(true)
   return (
     <>
+    <BrowserRouter>
     <Header/>
+    <Routes>
+        <Route exact path="/" component={Home} />
+        <Route path="/recipes" component={Recipes} />
+        <Route path="/favourites" component={Favourites} />
+      </Routes>
     <Home/>
     <Banner/>
     <RecipeLists />
-    {/* setLoader={setLoader} */}
-      {/* {loader && <div className='loader'>
-        <div className='spinner'></div>
-        </div>} */}
-    {/* <Community/> */}
+
     <DownloadApp />
     <Newsletter/>
     <Footer/>
+    </BrowserRouter>
     </>
   )
 }
